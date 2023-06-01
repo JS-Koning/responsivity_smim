a0378bd8a690f8fc5a782663cf42e6f1be6f64e5
JS-Koning <93373745+JS-Koning@users.noreply.github.com>
2023-06-01 12:55:53 +0200

    Update main.py
    removed unnecessary comments.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import numpy as np
 filename = 'table42.txt'
 
 admittances_array = functions.read_textfile(filename, skiprows=1)
-#print(admittances_array)                                            #look at textfile to see what is what. Does not really matter dont bother
+#print(admittances_array)                                            #look at textfile to see what is what.
 plot_real = functions.plots_epsilon(admittances_array, realpart=False)
